b933e31dcda0a7018eba7cb5fa15958ef33b3b90
Achim Turan <mail@achim-turan.de>
2016-03-10 19:50:03 +0100

    [CpyModesProcess] Initial commit of CpyModesProcess

---
+++ src/CpyModesProcess.cpp
@@ -0,0 +1,24 @@
+#include "CpyModesProcess.hpp"
+
+
+const std::string CCpyModesProcessName::ProcessName = "CCpyModesProcess";
+
+
+CCpyModesProcess::CCpyModesProcess()
+{
+}
+
+CCpyModesProcess::~CCpyModesProcess()
+{
+}
+
+
+AE_DSP_ERROR CCpyModesProcess::Create()
+{
+  return AE_DSP_ERROR_NO_ERROR;
+}
+
+AE_DSP_ERROR CCpyModesProcess::Destroy()
+{
+  return AE_DSP_ERROR_NO_ERROR;
+}
