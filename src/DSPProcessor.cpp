caf562de38f11c87816273a71cfb5bd1659012a4
AchimTuran <mail@achim-turan.de>
2016-07-11 17:13:04 +0200

    [Project] Adapt license header to GPL V3

--- src/DSPProcessor.cpp
+++ src/DSPProcessor.cpp
@@ -1,10 +1,10 @@
 /*
- *      Copyright (C) 2005-2014 Team KODI
+ *      Copyright (C) 2005-2016 Team KODI
  *      http://kodi.tv
  *
  *  This Program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2, or (at your option)
+ *  the Free Software Foundation; either version 3, or (at your option)
  *  any later version.
  *
  *  This Program is distributed in the hope that it will be useful,
